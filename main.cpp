9da05a60aab02f324403eaaaed90c15f6a40d6fc
msincos <msincos@outlook.com>
2021-07-18 10:40:37 +0800

    add iostream for print.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
+#include <iostream>
 #include <stdio.h>
 #include "main.h"
 
+using namespace std;
+
 int main(int argc, char* argv[])
 {
+	if (argc > 90)
+	{
+		cout << "you are cute." <<endl;
+		/* code */
+	}
+	
+	
 	return 0;
 }
 
